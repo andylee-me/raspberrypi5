46f43e5aabaf4aefcafb2967b33e34808fba24e2
andylee-me <60034043+andylee-me@users.noreply.github.com>
2024-07-17 19:37:15 +0800

    Update print.py
    add temp print

--- print.py
+++ print.py
@@ -1,4 +1,9 @@
 import time
-print("Hello")
-echo "#### Hello" >> $GITHUB_STEP_SUMMARY
+import psutil
+
+def get_cpu_temp():
+    temp = psutil.sensors_temperatures()['cpu_thermal'][0].current
+    return temp
+  
+print(get_cpu_temp())
 time.sleep(1)
